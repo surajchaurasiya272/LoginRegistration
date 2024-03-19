7ed17ade4b1d9e42029796c3095af440ffda535e
Suraj Chaurasiya <suraj.chaurasiya@coraltele.com>
2024-03-19 16:26:55 +0530

    surajjnnxcd

--- src/main/java/com/example/register/controller/Controller.java
+++ src/main/java/com/example/register/controller/Controller.java
@@ -1,20 +1,29 @@
 package com.example.register.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.register.model.User;
+import com.example.register.service.UserService;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
 
 @RestController
 @RequestMapping("/home")
 
 public class Controller {
+    @Autowired
+    private UserService user;
+
     @GetMapping("/users")
-    public String getMethodName() {
-        return "users";
+    public List<User> get() {
+        return user.getSet();
     }
-    
+
 }
--- src/main/java/com/example/register/service/UserService.java
+++ src/main/java/com/example/register/service/UserService.java
@@ -6,10 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.stereotype.Service;
+
 import com.example.register.model.User;
 
 
-
+@Service("UserService")
 public class UserService {
     private List<User> store =new ArrayList<>();
 
@@ -22,8 +24,8 @@ public class UserService {
         store.add(new User(Integer.parseInt(UUID.randomUUID().toString()) ,"ashok","ashok@gmail.com"));
     }
 
-    public List<User> service(){
-        
+    public List<User> getSet(){
+        System.out.println(store+"suraj chaursiya ");
+        return this.store;
     }
-
 }
