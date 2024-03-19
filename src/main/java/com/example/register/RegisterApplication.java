f9982376d4e2171ee20e1248235f4eeb6ce67906
surajchaurasiya272 <158546716+surajchaurasiya272@users.noreply.github.com>
2024-03-19 14:39:20 +0530

    Add files via upload

---
+++ src/main/java/com/example/register/RegisterApplication.java
@@ -0,0 +1,12 @@
+package com.example.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterApplication {g
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterApplication.class, args);
+	}
+
+}
